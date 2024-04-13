bf8b049f50dd309f2a60605eec413778189f893b
Patni_Vishvas <113787670+vishvasPatni@users.noreply.github.com>
2024-04-13 21:34:24 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,201 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 55) {
+                $('.fixed-top').addClass('shadow');
+            } else {
+                $('.fixed-top').removeClass('shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 55) {
+                $('.fixed-top').addClass('shadow').css('top', -55);
+            } else {
+                $('.fixed-top').removeClass('shadow').css('top', 0);
+            }
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 2
+            }
+        }
+    });
+
+
+    // epoke carousel
+    $(".vegetable-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Product Quantity
+    $('.quantity button').on('click', function () {
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
+})(jQuery);
+
+
+//Cross button
+
+document.addEventListener("DOMContentLoaded", function () {
+    var crossButtons = document.querySelectorAll(".btn-remove");
+    crossButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            var row = this.closest("tr");
+            row.parentNode.removeChild(row);
+        });
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all the plus and minus buttons
+    var plusButtons = document.querySelectorAll(".btn-plus");
+    var minusButtons = document.querySelectorAll(".btn-minus");
+
+    // Iterate through each plus button
+    plusButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Get the input field for quantity
+            var inputField = this.parentElement.nextElementSibling.querySelector("input");
+
+            // Increase the quantity by 1
+            inputField.value = parseInt(inputField.value) + 1;
+
+            // Update the total price
+            updateTotalPrice(this);
+        });
+    });
+
+    // Iterate through each minus button
+    minusButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Get the input field for quantity
+            var inputField = this.parentElement.previousElementSibling.querySelector("input");
+
+            // Ensure the quantity is not less than 1
+            if (parseInt(inputField.value) > 1) {
+                // Decrease the quantity by 1
+                inputField.value = parseInt(inputField.value) - 1;
+
+                // Update the total price
+                updateTotalPrice(this);
+            }
+        });
+    });
+});
